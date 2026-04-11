7d4fca2ccbd9d761f6d52e98d798cd7ef9ec7511
Ayutac <fly.high.android@gmail.com>
2026-04-11 19:40:41 +0200

    Removed a bit of duplicate code.

--- datagen/src/main/java/net/arna/jcraft/datagen/providers/data/JTagProviders.java
+++ datagen/src/main/java/net/arna/jcraft/datagen/providers/data/JTagProviders.java
@@ -100,14 +100,6 @@ public class JTagProviders {
 
             addCosplayTags();
 
-            getOrCreateRawBuilder(JTagRegistry.PROTECTS_FROM_SUN).addTag(JItemRegistry.KARS_HEADWRAP.getTag().location());
-            getOrCreateRawBuilder(JTagRegistry.PROTECTS_FROM_SUN).addTag(JItemRegistry.RED_HAT.getTag().location());
-            getOrCreateRawBuilder(JTagRegistry.PROTECTS_FROM_SUN).addTag(JItemRegistry.PUCCIS_HAT.getTag().location());
-            getOrCreateRawBuilder(JTagRegistry.PROTECTS_FROM_SUN).addTag(JItemRegistry.RISOTTO_CAP.getTag().location());
-            getOrCreateRawBuilder(JTagRegistry.PROTECTS_FROM_SUN).addTag(JItemRegistry.DIEGO_HAT.getTag().location());
-            getOrCreateRawBuilder(JTagRegistry.PROTECTS_FROM_SUN).addTag(JItemRegistry.MOUNTAIN_TIM_HAT.getTag().location());
-            getOrCreateRawBuilder(JTagRegistry.PROTECTS_FROM_SUN).addTag(JItemRegistry.COWBOY_HAT.getTag().location());
-
             final var cosplay = getOrCreateTagBuilder(JTagRegistry.COSPLAY);
             for (final CosplayItem<?> cosplayItem : CosplayItem.all()) {
                 cosplay.addTag(cosplayItem.getTag());
